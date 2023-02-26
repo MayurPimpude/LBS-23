fdbe24f8015cba515deec0b4405b2b7335e4d54c
abc <abc@example.com>
2023-02-26 17:21:22 +0530

    Initial Commit Vemburaj

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    // colors:{
+    //   'color1': 'rgb(184,212,68)',
+    // },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
